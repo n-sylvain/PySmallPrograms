b97570f3054f271e303fdac4ef9ccf6abb79873a
Sylvain Ni <sylvainni@msn.com>
2024-06-05 18:05:54 +0200

    Finish script example

--- script.py
+++ script.py
@@ -1,17 +1,16 @@
 # import math
-import sys
-
+# import sys
 # from os import rename
 
 import requests
 
-print(sys.version)
-print(sys.executable)
+# print(sys.version)
+# print(sys.executable)
 
 
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
+# def greet(who_to_greet):
+#     greeting = "Hello, {}".format(who_to_greet)
+#     return greeting
 
 
 r = requests.get("https://www.google.com")
